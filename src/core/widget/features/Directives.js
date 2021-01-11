0fd9a4f39dc0c8d556a617768d123a79b32d41df
Siddeshwar <siddeshwar.work@gmail.com>
2021-01-11 06:06:05 +0530

    made reactive-form work

--- src/core/widget/features/Directives.js
+++ src/core/widget/features/Directives.js
@@ -5,30 +5,32 @@ import { generateId } from '../../../shared/utility';
 
 class Directives extends Feature {
     run() {
+        const { widgetInst, childEl } = this;
+
         this.forEachAttrs(currentAttribute => {
             if (currentAttribute.name.charAt(0) === ':') {
                 const actualName = currentAttribute.name.substring(1, currentAttribute.name.length);
-                this.childEl.removeAttribute(currentAttribute.name);
+                childEl.removeAttribute(currentAttribute.name);
 
                 if (actualName === 'if') {
-                    const childElCopy = this.childEl;
+                    const childElCopy = childEl;
                     const replaceDummyId = generateId(16);
 
-                    const innerStringInterpolation = this.childEl.querySelector('kyte-container[for="si"]');
+                    const innerStringInterpolation = childEl.querySelector('kyte-container[for="si"]');
 
                     const execureDirective = function () {
                         let isTrue;
-                        eval(`isTrue = (${currentAttribute.value.replace('this.', 'this.widgetInst.')}) ? true: false`);
+                        eval(`isTrue = (${currentAttribute.value.replace('this.', 'widgetInst.')}) ? true: false`);
 
                         if (!isTrue) {
                             const replaceDummy = document.createElement('kyte-placeholder');
                             replaceDummy.setAttribute('id', replaceDummyId);
-                            this.childEl.replaceWith(replaceDummy);
+                            childEl.replaceWith(replaceDummy);
                         } else {
                             if (document.querySelector(`kyte-placeholder[id="${replaceDummyId}"]`)) {
                                 document.querySelector(`kyte-placeholder[id="${replaceDummyId}"]`).replaceWith(childElCopy);
                                 if (innerStringInterpolation) {
-                                    const innerStringInterpolationData = this.widgetInst._stringInterpolations.find(strIntr => strIntr.id === innerStringInterpolation.id);
+                                    const innerStringInterpolationData = widgetInst._stringInterpolations.find(strIntr => strIntr.id === innerStringInterpolation.id);
                                     innerStringInterpolationData.update();
                                 }
                             }
@@ -38,39 +40,39 @@ class Directives extends Feature {
                     execureDirective();
 
                     // Subscribe for the state changes
-                    this.widgetInst.$state.subscribe(Observable_Events.changed, () => {
+                    widgetInst.$state.subscribe(Observable_Events.changed, () => {
                         execureDirective();
                     });
 
                     // Subscribe for the attribute changes
-                    this.widgetInst.$attrs.subscribe(Observable_Events.changed, () => {
+                    widgetInst.$attrs.subscribe(Observable_Events.changed, () => {
                         execureDirective();
                     });
                 }
 
                 else if (actualName === 'hide') {
-                    const defaultDisplayType = this.childEl.style.display;
+                    const defaultDisplayType = childEl.style.display;
 
                     const execureDirective = function () {
                         let isTrue;
-                        eval(`isTrue = (${currentAttribute.value.replace('this.', 'this.widgetInst.')}) ? true: false`);
+                        eval(`isTrue = (${currentAttribute.value.replace('this.', 'widgetInst.')}) ? true: false`);
 
                         if (!isTrue) {
-                            this.childEl.style.display = 'none';
+                            childEl.style.display = 'none';
                         } else {
-                            this.childEl.style.display = defaultDisplayType;
+                            childEl.style.display = defaultDisplayType;
                         }
                     };
 
                     execureDirective();
 
                     // Subscribe for the state changes
-                    this.widgetInst.$state.subscribe(Observable_Events.changed, () => {
+                    widgetInst.$state.subscribe(Observable_Events.changed, () => {
                         execureDirective();
                     });
 
                     // Subscribe for the attribute changes
-                    this.widgetInst.$attrs.subscribe(Observable_Events.changed, () => {
+                    widgetInst.$attrs.subscribe(Observable_Events.changed, () => {
                         execureDirective();
                     });
                 }
