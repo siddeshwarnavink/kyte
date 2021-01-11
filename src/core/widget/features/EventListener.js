593aebc43da9d116a5699f530f509bda03229f12
Siddeshwar <siddeshwar.work@gmail.com>
2021-01-12 05:22:58 +0530

    added test for features/EventListener.js

--- src/core/widget/features/EventListener.js
+++ src/core/widget/features/EventListener.js
@@ -3,59 +3,86 @@ import Feature from './Feature';
 import { htmlContains } from '../../../shared/utility';
 
 class EventListener extends Feature {
+    checkForEventListner(currentAttribute) {
+        return currentAttribute.name.charAt(0) === '#';
+    }
+
+    getEventName(currentAttribute) {
+        return currentAttribute.name.substring(1);
+    }
+
+    checkForEventAlreadyRegistered(eventName) {
+        return this.widgetInst._eventListeners.find(event => (event.type === eventName && event.element.innerHTML.trim() === this.childEl.innerHTML.trim())) ? true : false;
+    }
+
+    bindEventGenerateStatePieceName(currentAttribute) {
+        return currentAttribute.value.substring(1, currentAttribute.value.length - 1).replace('this.', 'this.widgetInst.');
+    }
+
+    createAddEventFunction() {
+        return (eventName, eventHandler, childEl) => {
+            childEl.addEventListener(eventName, eventHandler);
+
+            this.widgetInst._eventListeners = [
+                ...this.widgetInst._eventListeners,
+                {
+                    type: eventName,
+                    onEmit: eventHandler,
+                    element: childEl
+                }
+            ];
+        };
+    }
+
+    createOtherEventHandlerFunction(currentAttribute) {
+        return (event) => {
+            const codeToRun = `${currentAttribute.value.substring(1, currentAttribute.value.length - 1).replace('this.', 'widgetInst.')}(event)`;
+            eval(codeToRun);
+        };
+    }
+
+    // if the attribute is for 'Two-way binding'
+    bindEventHandler(addEvent, currentAttribute) {
+        if (htmlContains(this.widgetInst.template, this.childEl.outerHTML)) {
+            const statePieceName = this.bindEventGenerateStatePieceName(currentAttribute);
+
+            this.childEl.value = eval(statePieceName);
+
+            // istanbul ignore next
+            addEvent('input', (event) => {
+                eval(`
+                 this.widgetInst.updateState(() => {
+                     ${statePieceName} = event.target.value;
+                 })
+             `);
+
+                this.childEl.value = event.target.value;
+            }, this.childEl);
+        } else {
+            return false;
+        }
+    }
+
+    otherEventHandler(currentAttribute, eventName, addEvent) {
+        const eventHandler = this.createOtherEventHandlerFunction(currentAttribute);
+        addEvent(eventName, eventHandler, this.childEl);
+    }
+
     run() {
-        const widgetInst = this.widgetInst;
+        const classInst = this;
 
-        this.forEachAttrs(currentAttribute => {
-            if (currentAttribute.name.charAt(0) === '#') {
-                const eventName = currentAttribute.name.substring(1);
+        classInst.forEachAttrs(currentAttribute => {
+            // istanbul ignore next
+            if (!classInst.checkForEventListner(currentAttribute)) {
+                const eventName = classInst.getEventName(currentAttribute);
 
-                // Check if the event is already registered
-                if (!widgetInst._eventListeners.find(event => (event.type === eventName && event.element.innerHTML.trim() === this.childEl.innerHTML.trim()))) {
-                    // Utility function for registering an event.
-                    const addEvent = function (eventName, eventHandler, childEl) {
-                        childEl.addEventListener(eventName, eventHandler);
+                if (classInst.checkForEventAlreadyRegistered(eventName)) {
+                    const addEvent = this.createAddEventFunction();
 
-                        widgetInst._eventListeners = [
-                            ...widgetInst._eventListeners,
-                            {
-                                type: eventName,
-                                onEmit: eventHandler,
-                                element: childEl
-                            }
-                        ];
-                    };
-
-                    // if the attribute is for 'Two-way binding'
                     if (eventName === 'bind') {
-                        // Making sure that the parrent widget is not handling
-                        if (htmlContains(widgetInst.template, this.childEl.outerHTML)) {
-                            // The state to which the input must be binded with
-                            const statePieceName = currentAttribute.value.substring(1, currentAttribute.value.length - 1).replace('this.', 'widgetInst.');
-
-                            // Setting the default value of the input
-                            this.childEl.value = eval(statePieceName);
-
-                            // Binding logic
-                            addEvent('input', (event) => {
-                                eval(`
-                                 widgetInst.updateState(() => {
-                                     ${statePieceName} = event.target.value;
-                                 })
-                             `);
-
-                                this.childEl.value = event.target.value;
-                            }, this.childEl);
-                        }
-
-                    }
-                    // Default event listener
-                    else {
-                        const eventHandler = () => {
-                            const codeToRun = `${currentAttribute.value.substring(1, currentAttribute.value.length - 1).replace('this.', 'widgetInst.')}(event)`;
-                            eval(codeToRun);
-                        };
-                        addEvent(eventName, eventHandler, this.childEl);
+                        classInst.bindEventHandler(addEvent, currentAttribute);
+                    } else {
+                        classInst.otherEventHandler(currentAttribute, eventName, addEvent);
                     }
                 }
             }
