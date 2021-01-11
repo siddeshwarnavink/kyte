c2b3b765a18164fd1ac13297ce1f00e82ec88015
Siddeshwar <siddeshwar.work@gmail.com>
2021-01-11 11:13:05 +0530

    added test for features/StringInterpolation.js

--- src/core/widget/features/StringInterpolation.js
+++ src/core/widget/features/StringInterpolation.js
@@ -4,50 +4,63 @@ import { Observable_Events } from '../../Observable';
 import { createExtractor, generateId } from '../../../shared/utility';
 
 class StringInterpolation extends Feature {
-    run() {
+    forEachInterpolation(callback) {
+        const stringInterpolationExtractor = createExtractor(['{{', '}}']);
+        const extracts = stringInterpolationExtractor(this.widgetInst.template);
+
+        extracts.forEach(callback);
+    }
+
+    generateCodeToRun(extract) {
+        return extract
+            .substring(2, extract.length - 2)
+            .replace('this.', 'widgetInst.')
+            .trim();
+    }
+
+    createKyteContaner(extract, codeToRun) {
+        const wrapperId = 'si_' + generateId(16);
+        const wrapper = `<kyte-container for="si" id="${wrapperId}">${eval(codeToRun)}</kyte-container>`;
+
+        this.childEl.innerHTML = this.childEl.innerHTML.replace(extract, wrapper);
+        return wrapperId;
+    }
+
+    createSubscribeForUpdateFunction(wrapperId, codeToRun) {
+        return () => {
+            if (document.querySelector(`kyte-container[id="${wrapperId}"]`)) {
+                document.querySelector(`kyte-container[id="${wrapperId}"]`).innerHTML = eval(codeToRun);
+            }
+        }
+    }
+
+    subscribeForUpdates(codeToRun, wrapperId) {
         const widgetInst = this.widgetInst;
 
-        // Creating an extractor
-        const stringInterpolationExtractor = createExtractor(['{{', '}}']);
-        // The extracted values
-        const extracts = stringInterpolationExtractor(widgetInst.template);
+        const update = this.createSubscribeForUpdateFunction(wrapperId, codeToRun);
 
-        // If there are any extracts
-        if (extracts.length > 0) {
-            // Looping through the collected extracts
-            extracts.forEach(extract => {
-                // The code to eval() to get the actual value
-                const codeToRun = extract
-                    .substring(2, extract.length - 2)
-                    .replace('this.', 'widgetInst.')
-                    .trim();
+        widgetInst._stringInterpolations.push({
+            id: wrapperId,
+            update
+        });
 
-                const wrapperId = 'si_' + generateId(16);
-                this.childEl.innerHTML = this.childEl.innerHTML.replace(
-                    extract,
-                    `<kyte-container for="si" id="${wrapperId}">${eval(codeToRun)}</kyte-container>`
-                );
+        // Subscribe for the state changes
+        widgetInst.$state.subscribe(Observable_Events.changed, update);
 
+        // Subscribe for the attribute changes
+        widgetInst.$attrs.subscribe(Observable_Events.changed, update);
+    }
 
-                // Replace with new value
-                function update() {
-                    if (document.querySelector(`kyte-container[id="${wrapperId}"]`)) {
-                        document.querySelector(`kyte-container[id="${wrapperId}"]`).innerHTML = eval(codeToRun);
-                    }
-                }
+    run() {
+        const classInst = this;
 
-                widgetInst._stringInterpolations.push({
-                    id: wrapperId,
-                    update
-                });
+        this.forEachInterpolation((extract) => {
+            const codeToRun = classInst.generateCodeToRun(extract);
 
-                // Subscribe for the state changes
-                widgetInst.$state.subscribe(Observable_Events.changed, update);
+            const wrapperId = classInst.createKyteContaner(codeToRun);
 
-                // Subscribe for the attribute changes
-                widgetInst.$attrs.subscribe(Observable_Events.changed, update);
-            });
-        }
+            classInst.subscribeForUpdates(codeToRun, wrapperId);
+        });
     }
 }
 
