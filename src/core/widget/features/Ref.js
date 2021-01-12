86a4583c75bd38415622df29270165416ad83319
Siddeshwar <siddeshwar.work@gmail.com>
2021-01-12 06:10:40 +0530

    added test for features/Ref.js

--- src/core/widget/features/Ref.js
+++ src/core/widget/features/Ref.js
@@ -1,31 +1,45 @@
 import Feature from './Feature';
 
 class Ref extends Feature {
+    isRef(currentAttribute) {
+        return currentAttribute.name === 'ref';
+    }
+
+    isCustomWidget() {
+        return Object.keys(this.widgetInst.widgets).indexOf(this.childEl.localName) > -1;
+    }
+
+    handleIfNativeElement(currentAttribute) {
+        this.widgetInst.refs[currentAttribute.value] = {
+            dom: this.childEl,
+            isCustomWidget: false
+        };
+    }
+
+    handleIfCustomWidget(currentAttribute) {
+        this.widgetInst._customWidgets.forEach(cWidget => {
+            // Finding the selected widget instance
+            if (this.childEl.children[0].attributes.id.value === cWidget.id) {
+                const widget = cWidget.instance;
+
+                this.widgetInst.refs[currentAttribute.value] = {
+                    dom: this.childEl,
+                    isCustomWidget: true,
+                    widget
+                };
+            }
+        });
+    }
+
     run() {
+        const classInst = this;
+
         this.forEachAttrs(currentAttribute => {
-            if (currentAttribute.name === 'ref') {
-                // Check if it is a custom widget
-                const isCustomWidget = Object.keys(this.widgetInst.widgets).indexOf(this.childEl.localName) > -1;
-
-                if (!isCustomWidget) {
-                    this.widgetInst.refs[currentAttribute.value] = {
-                        dom: this.childEl,
-                        isCustomWidget: false
-                    };
+            if (classInst.isRef(currentAttribute)) {
+                if (!classInst.isCustomWidget()) {
+                    classInst.handleIfNativeElement(currentAttribute);
                 } else {
-                    // Looping throught all the registered widgets
-                    this.widgetInst._customWidgets.forEach(cWidget => {
-                        // Finding the selected widget instance
-                        if (this.childEl.children[0].attributes.id.value === cWidget.id) {
-                            const widget = cWidget.instance;
-
-                            this.widgetInst.refs[currentAttribute.value] = {
-                                dom: this.childEl,
-                                isCustomWidget: true,
-                                widget
-                            };
-                        }
-                    });
+                    classInst.handleIfCustomWidget(currentAttribute);
                 }
             }
         });
